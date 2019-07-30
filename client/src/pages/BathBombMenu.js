01d075001f2ba8bf91b2672b0db73940d4935839
Meghan Bomberger <44277473+MeghanBomberger@users.noreply.github.com>
2019-07-30 01:46:29 -0600

    update

---
+++ client/src/pages/BathBombMenu.js
@@ -0,0 +1,52 @@
+import React from 'react'
+
+import '../styles/BathBombMenu.css'
+
+const BathBombMenu = () => {
+    return (
+        <main className="bathbombmenu-page page-container">
+            <div className="bathbomb-card-list">
+                
+            </div>
+        </main>
+    )
+}
+
+export default BathBombMenu
+
+// TODO LIST
+    // Images
+        // Bath Bomb Bases
+            // sphere
+            // circle
+            // oval
+            // rectangle
+            // square
+            // gem
+            // mushroom
+            // fox
+            // round flower
+            // point flower
+        // data entry
+            // seed list x5
+            // full list
+        // zoom toggle
+        // action
+            // single color
+            // dual color
+            // single color burst
+            // dual color burst
+        // genre sort
+            // fantasy
+            // scifi
+            // anime
+            // cartoon
+            // video games
+        // scent sort
+            // floral
+            // tea based
+            // fruity
+            // bakery
+            // woodsy
+            // beachy
+
